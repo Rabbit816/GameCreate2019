dadc42482faca996bcb686d11f764cee1e226cb8
KazunoriHara <17c1020021hk@edu.tech.ac.jp>
2019-05-11 18:52:52 +0900

    スクリプト追加

---
+++ Game Project/Assets/Scripts/View/CardView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField]
+    private int cardId;
+    public int CardId { set { cardId = value; } }
+    [SerializeField]
+    private Image cardImage;
+    
+    public void CardColor()
+    {
+        switch (cardId)
+        {
+            case 0:
+                cardImage.color = Color.red;
+                break;
+            case 1:
+                cardImage.color = Color.blue;
+                break;
+            case 2:
+                cardImage.color = Color.yellow;
+                break;
+            case 3:
+                cardImage.color = Color.green;
+                break;
+            case 4:
+                cardImage.color = Color.black;
+                break;
+        }
+    }
+}
