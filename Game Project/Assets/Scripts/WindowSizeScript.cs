2e802de4c50bbf06d4007a23cdb55048b9679458
Kazunori Hara <17c1020021hk@edu.tech.ac.jp>
2019-08-23 06:24:05 +0900

    ビルド関連の設定

--- Game Project/Assets/Scripts/WindowSizeScript.cs
+++ Game Project/Assets/Scripts/WindowSizeScript.cs
@@ -7,6 +7,6 @@ public class WindowSizeScript : MonoBehaviour {
     [RuntimeInitializeOnLoadMethod]
     static void OnRuntimeMethodLoad()
     {
-        Screen.SetResolution(1920, 1080, false, 60);
+        Screen.SetResolution(1920, 1080, true, 60);
     }
 }
